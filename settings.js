9d08905f6cf6e4ad78fb32a9532aaa47c52329c8
Lenoxlynon <148803888+Lenoxlynon@users.noreply.github.com>
2026-04-18 12:17:23 +0200

    Add media sending configuration to settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,12 @@ const settings = {
   botName: "𝗧𝗠𝗧-𝗫𝗠𝗗-𝗕𝗢𝗧",
   botOwner: 'Professor', // Your name
   ownerNumber: '27749797749', //Set your number here without + symbol, just add country code & number without any space
+  // Enable media sending
+module.exports = {
+    allowMedia: true,
+    maxVideoSize: 50 * 1024 * 1024, // 50MB limit
+    // ... other configs
+};
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
